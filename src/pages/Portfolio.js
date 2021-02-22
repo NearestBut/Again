7701b9e51735b3198cb38c8678965625489f10d6
brjeong <brjeong0@gmail.com>
2021-02-22 19:43:51 +0900

    수정

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 
 const Portfolio = () => {
     return (
-        <h1>포트폴리오!!</h1>
-        <h2>폴프폴리오만들어야합니다!!!</h2> 
+        <div>
+            <h1>포트폴리오!!</h1>
+            <h2>폴프폴리오만들어야합니다!!!</h2> 
+        </div>
     );
 }
 
