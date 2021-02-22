b6efda95f2aed7ea44602eb333346a0b8591e0aa
brjeong <brjeong0@gmail.com>
2021-02-22 19:45:55 +0900

    틀린거다

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 const Portfolio = () => {
     return (
-        <div>
+        
             <h1>포트폴리오!!</h1>
-            <h2>폴프폴리오만들어야합니다!!!!!!!!!!!!!!!</h2> 
-        </div>
+            <h2>틀렸다!!!!!!!!!!!</h2> 
+        
     );
 }
 
