0c799e1d39030d531d27da92b8733b83821c73e8
yoonho91 <yoonhocho91@gmail.com>
2021-02-22 20:36:01 +0900

    3modi

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -8,6 +8,13 @@ const Profile = () => {
             <h1>안녕 날 소개하지</h1>
             <h2>내 직업은 빠가사리 사리사리 빠가사리</h2>
         </Component>
+        <Component>
+            <h1>나의 이력</h1>
+            <h2>바보가 된지 31년 쨰 입니다,</h2>
+        </Component>
+        <Component>
+            <h1>나의 장점</h1>
+        </Component>
               </div>
     );
 }
