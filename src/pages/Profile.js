6c5f2acace0edb7e90c00db51906373004825380
yoonseohuh <yshuh718@gmail.com>
2021-02-22 20:51:39 +0900

    수정

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 const Profile = () => {
     return (
-        <h1>프로필!!</h1>
         <div>
+          <h1>프로필!!</h1>
           <Component>
               <h1>안녕 날 소개하지</h1>
               <h2>내 직업은 빠가사리 사리사리 빠가사리</h2>
