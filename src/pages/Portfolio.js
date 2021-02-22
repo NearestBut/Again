fe2d96e709a00231df42457349315ca72ff5c382
brjeong <brjeong0@gmail.com>
2021-02-22 21:20:56 +0900

    구조대강

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,8 +3,19 @@ import React, { Component } from 'react';
 const Portfolio = () => {
     return (
         <div>
-            <h1>포트폴리오!!</h1>
-            <h2>테스트 3번째다</h2> 
+            <div>
+                <h1>사진</h1>
+            </div>
+            <div>
+                1차 프로젝트
+                <link>git</link>
+                <link>pdf</link><br/>
+                2020.01.01~2020.01.31 <br/>
+                소개글<br/>
+                개발환경<br/>
+                참여도<br/>
+                
+            </div>
         </div>
     );
 }
