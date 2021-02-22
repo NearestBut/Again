0010dd51542f10bf65d5e74f51b54840f2c9581e
yoonseohuh <yshuh718@gmail.com>
2021-02-22 20:26:47 +0900

    불필요파일제거

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 const Portfolio = () => {
     return (
-        
+        <div>
             <h1>포트폴리오!!</h1>
             <h2>풀리퀘 왜안뜨냐?</h2> 
-        
+        </div>
     );
 }
 
