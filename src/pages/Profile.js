9ef48add7a2bb8dc56af4f762b7820a8d0e9bccb
yoonho91 <yoonhocho91@gmail.com>
2021-02-22 20:51:21 +0900

    4modi

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const Profile = () => {
     return (
         <div>
-        <h1>프로필!!</h1>
+            <h1>프로필!!</h1>
         <Component>
             <h1>안녕 날 소개하지</h1>
             <h2>내 직업은 빠가사리 사리사리 빠가사리</h2>
