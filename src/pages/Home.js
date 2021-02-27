74bb052a2bcbb038ac7e4739f4420d90128cbe56
yoonseohuh <yshuh718@gmail.com>
2021-02-27 15:29:24 +0900

    차트

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
-import '../css/reset.css';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import Chart from 'chart.js'
 
 const Home = () => {
+    
     return (
         <div>
             <h1>홈</h1>
             <div className="skillBar">
-                <h2>LANGUAGE</h2>
+                <h2>SKILL</h2>
+                <h3>LANGUAGE</h3>
                 Java <progress value="70" max="100"></progress><br/>
                 Javascript <progress value="50" max="100"></progress><br/>
-                <h2>FRAMEWORK</h2>
+                <h3>FRAMEWORK</h3>
                 Spring <progress value="70" max="100"></progress><br/>
                 Node.js <progress value="50" max="100"></progress><br/>
                 React.js <progress value="50" max="100"></progress><br/>
-                <h2>DATABASE</h2>
+                <h3>DATABASE</h3>
                 MyBatis <progress value="70" max="100"></progress><br/>
-                <h2>etc.</h2>
+                <h3>etc.</h3>
                 Git <progress value="70" max="100"></progress><br/>
             </div>
             <div className="pfPreview">
-                <h2></h2>
+                <h2>PORTFOLIO PREVIEW</h2>
+                <Chart/>
             </div>
             <div className="contact">
+                <h2>CONTACT</h2>
+                <ul>
+                    <li><b>email</b> &nbsp; yshuh718@gmail.com</li>
+                    <li>
+                        <b>github</b> &nbsp; <a href="http://github.com/yoonseohuh">github.com/yoonseohuh</a>
+                    </li>
+                </ul>
             </div>
         </div>
     );
