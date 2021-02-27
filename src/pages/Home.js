b1f8aaff354f1a6bd013712b2bf2b955d32d03a9
yoonseohuh <yshuh718@gmail.com>
2021-02-27 14:23:16 +0900

    d

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import '../css/reset.css';
 
 const Home = () => {
     return (
         <div>
             <h1>홈</h1>
-            <div class="skillBar">
+            <div className="skillBar">
                 <h2>LANGUAGE</h2>
                 Java <progress value="70" max="100"></progress><br/>
                 Javascript <progress value="50" max="100"></progress><br/>
@@ -13,27 +14,14 @@ const Home = () => {
                 Node.js <progress value="50" max="100"></progress><br/>
                 React.js <progress value="50" max="100"></progress><br/>
                 <h2>DATABASE</h2>
-                Java <progress value="70" max="100"></progress><br/>
-                Javascript <progress value="50" max="100"></progress><br/>
+                MyBatis <progress value="70" max="100"></progress><br/>
                 <h2>etc.</h2>
                 Git <progress value="70" max="100"></progress><br/>
             </div>
-            <div class="pfPreview">
+            <div className="pfPreview">
                 <h2></h2>
             </div>
-            <div class="contact">
-                <table>
-                    <tr>
-                        <td>email</td>
-                    </tr>
-                    <tr>
-                        <td>@gmail.com</td>
-                    </tr>
-                    <tr>
-                        <td>github</td>
-                        <td></td>
-                    </tr>
-                </table>
+            <div className="contact">
             </div>
         </div>
     );
