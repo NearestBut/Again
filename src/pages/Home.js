4f7dba40365d69d7c83249ce992f0aebe13f016d
yoonseohuh <yshuh718@gmail.com>
2021-02-27 12:32:07 +0900

    틀

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
+
 const Home = () => {
     return (
         <div>
             <h1>Home</h1>
-            <table>
-                <tr border="0">
-                    <td><NavLink to="/portfolio">포트폴리오</NavLink></td>
-                    <td><NavLink to="/profile">프로필</NavLink></td>
-                    <td><NavLink to="/board">방명록</NavLink></td>
-                </tr>
-            </table>
+            <ul>
+                <li><NavLink to="/portfolio">포트폴리오</NavLink></li>
+                <li><NavLink to="/profile">프로필</NavLink></li>
+                <li><NavLink to="/board">방명록</NavLink></li>
+            </ul>
+            <div class="skillBar">
+                <h2>LANGUAGE</h2>
+                Java <progress value="70" max="100"></progress>
+                Javascript <progress value="50" max="100"></progress>
+                <h2>FRAMEWORK</h2>
+                Spring <progress value="70" max="100"></progress>
+                Node.js <progress value="50" max="100"></progress>
+                React.js <progress value="50" max="100"></progress>
+                <h2>DATABASE</h2>
+                Java <progress value="70" max="100"></progress>
+                Javascript <progress value="50" max="100"></progress>
+                <h2>etc.</h2>
+                Git <progress value="70" max="100"></progress>
+            </div>
+            <div class="pfPreview">
+                <h2></h2>
+            </div>
+            <div class="contact">
+                <table>
+                    <tr>
+                        <td>email</td>
+                    </tr>
+                    <tr>
+                        <td>@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>github</td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
         </div>
     );
 }
