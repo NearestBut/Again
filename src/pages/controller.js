acbda40cf2b21ac3529a550336e0a16aa8c59bfa
username <user@email.mail>
2021-02-27 13:29:30 +0900

    ㅇㅇ

--- src/pages/controller.js
+++ src/pages/controller.js
@@ -2,3 +2,4 @@ export {default as Home} from './Home';
 export {default as Profile} from './Profile';
 export {default as Portfolio} from './Portfolio';
 export {default as Board} from './Board';
+export {default as Footer} from './Footer';
