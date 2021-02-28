df1f48f1778076f32f6a7372983cc0756d81bd91
smlee111 <vic_sm@naver.com>
2021-02-28 17:57:57 +0900

    폰트적용

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -4,15 +4,17 @@ import { NavLink } from 'react-router-dom';
 class Top extends Component {
     render () {
         return(
-            <div className="top">
-                <p className="logo"></p>
-                <ul className="menu">
-                    <li><NavLink to="/Again/">Home</NavLink></li>
-                    <li><NavLink to="/Again/portfolio">Portfolio</NavLink></li>
-                    <li><NavLink to="/Again/profile">Profile</NavLink></li>
-                    <li><NavLink to="/Again/board">Board</NavLink></li>
-                    <li><NavLink to="/Again/Home2">Home2</NavLink></li>
-                </ul>
+            <div className="pages">
+                <div className="top">
+                    <p className="logo"></p>
+                    <ul className="menu">
+                        <li><NavLink to="/Again/">Home</NavLink></li>
+                        <li><NavLink to="/Again/portfolio">Portfolio</NavLink></li>
+                        <li><NavLink to="/Again/profile">Profile</NavLink></li>
+                        <li><NavLink to="/Again/board">Board</NavLink></li>
+                        <li><NavLink to="/Again/Home2">Home2</NavLink></li>
+                    </ul>
+                </div>
             </div>
         );
     }
