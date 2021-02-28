0ecfd5db1cb5bca49b6c9e2160820af7bdfb3bb6
smlee111 <vic_sm@naver.com>
2021-03-01 01:12:13 +0900

    홈css제작3

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,8 +3,7 @@ import React, { Component } from 'react';
 const Profile = () => {
     return (
         <div>
-          <h1>프로필!!</h1>
-            asdfasdfsdf
+            <h1>프로필!!</h1>
         </div>
     );
 }
--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,22 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-class Top extends Component {
+const useScroll = () => {
+    const [state, setState] = useState({
+      x: 0, // x와 y의 초기값을 0으로 지정
+      y: 0
+    });
+    const onScroll = () => {
+      setState({ x: window.scrollX, y: window.scrollY });
+    };
+    useEffect(() => {
+      window.addEventListener("scroll", onScroll); // scorll할 때 onScroll 이벤트 핸들러 지정
+      return () => window.removeEventListener("scroll", onScroll); // clean up
+    }, []);
+    return state;
+};
 
-    render(){
-        return(
-            <div className="top">
-                <p className="logo"></p>
-                <ul className="menu">
-                    <li><NavLink to="/Again/">Home</NavLink></li>
-                    <li><NavLink to="/Again/portfolio">Portfolio</NavLink></li>
-                    <li><NavLink to="/Again/profile">Profile</NavLink></li>
-                    <li><NavLink to="/Again/board">Board</NavLink></li>
-                    <li><NavLink to="/Again/Home2">ddddddddddd</NavLink></li>
-                </ul>
-            </div>
-        );
-    }
+const Top = () => {
+    const { y } = useScroll();
+    return(
+        <div className="top">
+            <p className="logo"></p>
+            <ul className="menu">
+                <li><NavLink to="/Again/">Home</NavLink></li>
+                <li><NavLink to="/Again/portfolio">Portfolio</NavLink></li>
+                <li><NavLink to="/Again/profile">Profile</NavLink></li>
+                <li><NavLink to="/Again/board">Board</NavLink></li>
+                <li><NavLink to="/Again/Home2">ddddddddddd</NavLink></li>
+            </ul>
+        </div>
+    );
 }
 
 export default Top;
