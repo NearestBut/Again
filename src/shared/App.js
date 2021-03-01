da189627ed0c745dfe6203eda15b0de0d30dba1d
smlee111 <vic_sm@naver.com>
2021-03-01 23:21:23 +0900

    반응형 1800 메인 이미지

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import 'controller/ctrl_resources';
-import { Top, Footer, Home, Profile, Portfolio, Board, Home2 } from 'controller/ctrl_pages';
+import { Top, Footer, Home, Profile, Portfolio, Board, Home2, Board2 } from 'controller/ctrl_pages';
 
 function isElementUnderBottom(elem, triggerDiff) {
     const { top } = elem.getBoundingClientRect();
@@ -53,6 +53,7 @@ class App extends Component {
                 <Route path="/Again/portfolio" component={Portfolio} />
                 <Route path="/Again/board" component={Board} />
                 <Route exact path="/Again/Home2" component={Home2} />
+                <Route exact path="/Again/Board2" component={Board2} />
                 <Footer/>
             </div>
         );
