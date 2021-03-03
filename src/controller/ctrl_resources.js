d66e3e21826dde41b50e6531f0f1ce6ad0087538
username <user@email.mail>
2021-03-03 09:35:34 +0900

    0302 방명록 구조

--- src/controller/ctrl_resources.js
+++ src/controller/ctrl_resources.js
@@ -4,6 +4,8 @@
 import 'resources/css/reset.css';
 import 'resources/css/common.css';
 import 'resources/css/shared.css';
+import 'resources/css/board2.css';
+
 
 // JS
 import 'resources/js/chart';
--- src/pages/Board2.js
+++ src/pages/Board2.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-import 'resources/css/board.css';
+import 'resources/css/board2.css';
 
 const Board2 = () => {
     return (
         <div className="pages" style={{'background':'#ccc','padding-top':'95px'}}>
+                <div className="top_visual">
+                    <div className="title">
+                        <h2>방명록 까지 오시느라 고생하셨습니다.</h2>
+                        <p>오늘의 기분은 어떠세요? 날씨는요? 아침은 드셨나요?</p>
+                        <p>작은 이야기라도 좋습니다. </p>
+                        <p>플라밍고는 당신의 모든 것이 궁금하거든요.</p>
+                    </div>
+                </div>
+                
         </div>
     );
 }
